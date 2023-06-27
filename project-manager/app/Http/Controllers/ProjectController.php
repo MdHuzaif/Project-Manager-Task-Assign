8f9efcd86cbd95174dc9b9c3e3019e519030afe9
MdHuzaif <enanamin002429@gmail.com>
2023-06-27 15:47:50 +0600

    v-1.1.2

--- project-manager/app/Http/Controllers/ProjectController.php
+++ project-manager/app/Http/Controllers/ProjectController.php
@@ -40,7 +40,48 @@ class ProjectController extends Controller
      return redirect()->back()->with('success', 'Project deleted successfully.');
    }
 
-    public function filter(Request $request)
+ public function filter(Request $request)
+{
+    $assignFromDate = $request->input('assign_from_date');
+    $assignToDate = $request->input('assign_to_date');
+    $releaseFromDate = $request->input('release_from_date');
+    $releaseToDate = $request->input('release_to_date');
+
+    $assignSum = Project::where(function ($query) use ($assignFromDate, $assignToDate, $releaseFromDate, $releaseToDate) {
+        $query->whereBetween('project_assign_date', [$assignFromDate, $assignToDate])
+            ->orWhere(function ($query) use ($assignFromDate, $releaseFromDate, $releaseToDate) {
+                $query->where('project_assign_date', '<', $assignFromDate)
+                    ->whereBetween('project_release_date', [$releaseFromDate, $releaseToDate]);
+            });
+    })->sum('project_amount');
+
+     $releaseSum = Project::whereBetween('project_release_date', [$releaseFromDate, $releaseToDate])->sum('project_amount');
+
+    // Exclude projects assigned after the last date of the assign date range from release sum
+    $releaseSum -= Project::where('project_assign_date', '>', $assignToDate)
+        ->whereBetween('project_release_date', [$releaseFromDate, $releaseToDate])
+        ->sum('project_amount');
+
+    $percentage = 0; // Default value in case $assignSum is zero
+    if ($assignSum != 0) {
+        $percentage = ($releaseSum / $assignSum) * 100;
+    }
+
+    $projects = Project::where(function ($query) use ($assignFromDate, $assignToDate, $releaseFromDate, $releaseToDate) {
+        $query->where(function ($query) use ($assignFromDate, $assignToDate) {
+            $query->where('project_assign_date', '>=', $assignFromDate)
+                ->where('project_assign_date', '<=', $assignToDate);
+        })->orWhere(function ($query) use ($releaseFromDate, $releaseToDate) {
+            $query->where('project_release_date', '>=', $releaseFromDate)
+                ->where('project_release_date', '<=', $releaseToDate);
+        });
+    })->get();
+
+    return view('projects.filter', compact('projects', 'percentage'));
+}
+
+
+    public function testfilter(Request $request)
     {
         $assignFromDate = $request->input('assign_from_date');
         $assignToDate = $request->input('assign_to_date');
@@ -60,5 +101,5 @@ class ProjectController extends Controller
         ->get();
 
     return view('projects.filter', compact('projects', 'percentage'));
-    }
+    }  
 }
